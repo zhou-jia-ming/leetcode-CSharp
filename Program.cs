e32f21f285b5c8f55e256809b80d43f49a0c1c34
Jiaming <zhoujiaming12345@gmail.com>
2020-07-16 22:31:39 +0800

    add git ignore and mstest package

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetcode_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
